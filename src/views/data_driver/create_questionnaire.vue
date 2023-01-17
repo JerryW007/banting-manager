03e69172dbd902d48bd9b71b891061e2f9dd8e1c
JerryW007 <726946445@qq.com>
2023-01-17 18:23:53 +0800

    update:去掉注释代码

--- src/views/data_driver/create_questionnaire.vue
+++ src/views/data_driver/create_questionnaire.vue
@@ -118,33 +118,12 @@ export default {
     draggable,
   },
   created() {
-    // this.enterEvent();
     this.getTableOptions();
   },
   mounted() {},
   updated() {},
   watch: {
     disease_id: function () {
-      // if (this.questions.length > 0) {
-      //   this.$confirm("此操作将会清空已设置的问卷题目, 是否继续?", "提示", {
-      //     confirmButtonText: "确定",
-      //     cancelButtonText: "取消",
-      //     type: "warning",
-      //   })
-      //     .then(() => {
-      //       this.questions = [];
-      //       this.$message({
-      //         type: "success",
-      //         message: "删除成功!",
-      //       });
-      //     })
-      //     .catch(() => {
-      //       this.$message({
-      //         type: "info",
-      //         message: "已取消删除",
-      //       });
-      //     });
-      // }
       this.getTableOptions();
     },
     "column.table_name": function () {
